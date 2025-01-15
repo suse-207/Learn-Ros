c443c00c3c16d9d301bd3920a15f2f05ae2646a1
R-207 <18708364532@163.com>
2025-01-15 12:17:20 +0000

    02_runtime_init

---
+++ 02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/kernel-3df560ebf67618c0.d
@@ -0,0 +1,14 @@
+/workspaces/R-os/02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/kernel-3df560ebf67618c0: src/main.rs src/bsp.rs src/bsp/raspberrypi.rs src/bsp/raspberrypi/cpu.rs src/cpu.rs src/_arch/aarch64/cpu.rs src/cpu/boot.rs src/cpu/../_arch/aarch64/cpu/boot.rs src/panic_wait.rs src/cpu/../_arch/aarch64/cpu/boot.s
+
+/workspaces/R-os/02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/kernel-3df560ebf67618c0.d: src/main.rs src/bsp.rs src/bsp/raspberrypi.rs src/bsp/raspberrypi/cpu.rs src/cpu.rs src/_arch/aarch64/cpu.rs src/cpu/boot.rs src/cpu/../_arch/aarch64/cpu/boot.rs src/panic_wait.rs src/cpu/../_arch/aarch64/cpu/boot.s
+
+src/main.rs:
+src/bsp.rs:
+src/bsp/raspberrypi.rs:
+src/bsp/raspberrypi/cpu.rs:
+src/cpu.rs:
+src/_arch/aarch64/cpu.rs:
+src/cpu/boot.rs:
+src/cpu/../_arch/aarch64/cpu/boot.rs:
+src/panic_wait.rs:
+src/cpu/../_arch/aarch64/cpu/boot.s:
