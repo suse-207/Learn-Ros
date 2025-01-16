af58d4520f6ad11953aad8b12cf0d6b8995bc10c
R-207 <18708364532@163.com>
2025-01-16 02:02:30 +0000

    Update Cargo.lock and remove generated files

--- 02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/some_crate-559d38ce1309710a.d
+++
@@ -1,7 +0,0 @@
-/workspaces/R-os/02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/libsome_crate-559d38ce1309710a.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/some_crate-0.2.1/src/lib.rs
-
-/workspaces/R-os/02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/libsome_crate-559d38ce1309710a.rlib: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/some_crate-0.2.1/src/lib.rs
-
-/workspaces/R-os/02_runtime_init/target/aarch64-unknown-none-softfloat/release/deps/some_crate-559d38ce1309710a.d: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/some_crate-0.2.1/src/lib.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/some_crate-0.2.1/src/lib.rs:
