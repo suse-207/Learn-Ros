f49f7c16d8bfa2101232e4494067c24ac48ac36f
R-207 <18708364532@163.com>
2025-03-20 11:36:28 +0000

    03

---
+++ 03_hacky_hello_world/src/print.rs
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2018-2023 Andre Richter <andre.o.richter@gmail.com>
+
+use crate::console;
+use core::fmt;
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use console::interface::Write;
+
+    console::console().write_fmt(args).unwrap();
+}
+
+/// Print without a newline.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::print::_print(format_args!($($arg)*)));
+}
+/// Print without a newline.
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print::_print(format_args!("{}\n", format_args!($($arg)*))));
+}
+
+
+
+
